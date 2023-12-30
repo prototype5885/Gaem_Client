75b8d77b609341a54d60f40ac633f07806e781ec
prototype5885 <prototype5885@gmail.com>
2023-12-30 01:42:31 +0100

    XD

--- Scripts/Database.cs
+++ Scripts/Database.cs
@@ -1,28 +1,82 @@
 using Godot;
-using System.Data.SQLite;
+using System;
+using Microsoft.Data.Sqlite;
 
 public partial class Database : Node
 {
     public override void _Ready()
     {
-        //string dbpath = OS.GetUserDataDir() + "/database.sqlite";
-        //GD.Print(dbpath);
-        SQLiteConnection.CreateFile("database.sqlite");
-        //SQLiteConnection dbConnection = new SQLiteConnection("Data Source=database.db;Version=3;");
-        //string idk = "Data Source=" + dbpath;
-        //GD.Print(idk);
-        SQLiteConnection dbConnection = new SQLiteConnection("database.sqlite");
-        dbConnection.Open();
-        GD.Print("wtf");
 
-        //string sql = "create table highscores (name varchar(20), score int)";
-        //SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
-        //command.ExecuteNonQuery();
+        SqliteConnectionStringBuilder builder = new SqliteConnectionStringBuilder();
+        builder.DataSource = OS.GetUserDataDir() + "\\database.db";
 
-        //sql = "insert into highscores (name, score) values ('Eric', 9002)";
-        //command = new SQLiteCommand(sql, dbConnection);
-        //command.ExecuteNonQuery();
+        string connectionString = builder.ConnectionString;
 
-        //dbConnection.Close();
+        GD.Print(builder.DataSource);
+
+        using (SqliteConnection dbConnection = new SqliteConnection(connectionString))
+            try
+            {
+                // Open the connection
+                dbConnection.Open();
+
+                // Create a table
+                CreatePlayersTable(dbConnection);
+
+
+                // Insert some data into the table
+                InsertData(dbConnection, 1, "John Doe", 25);
+                InsertData(dbConnection, 2, "Jane Doe", 32);
+
+                // Query and display data
+                QueryAndDisplayData(dbConnection);
+
+
+                GD.Print("Database connection opened successfully.");
+            }
+            catch (Exception ex)
+            {
+                GD.Print("Error opening database connection: " + ex.Message);
+            }
     }
+    void CreatePlayersTable(SqliteConnection dbConnection)
+    {
+        using (SqliteCommand command = new SqliteCommand("CREATE TABLE IF NOT EXISTS Players (ID INTEGER PRIMARY KEY, Name TEXT, Money INTEGER);", dbConnection))
+        {
+            command.ExecuteNonQuery();
+        }
+    }
+
+    void InsertData(SqliteConnection dbConnection, int id, string name, int money)
+    {
+        using (SqliteCommand command = new SqliteCommand("INSERT INTO Players (ID, Name, Money) VALUES (@id, @name, @money);", dbConnection))
+        {
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@money", money);
+
+            command.ExecuteNonQuery();
+        }
+    }
+
+    void QueryAndDisplayData(SqliteConnection dbConnection)
+    {
+        using (SqliteCommand command = new SqliteCommand("SELECT * FROM Players;", dbConnection))
+        {
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                string text = "";
+                GD.Print("Players table data:");
+                while (reader.Read())
+                {
+                    text = text + $"ID: {reader["ID"]}, Name: {reader["Name"]}, Money: {reader["Money"]}\n";
+                    GD.Print(text);
+                    Label label = GetNode<Label>("/root/Map/Player/HUD/Info2");
+                    label.Text = text;
+
+                }
+            }
+        }
+    }
+
 }
